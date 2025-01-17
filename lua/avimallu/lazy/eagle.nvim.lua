7b1e9371e88ff5922f73fa9bfed044e1c27fd360
Avinash Mallya <avimallu@protonmail.ch>
2025-01-17 08:16:43 -0600

    Add eagle.nvim

---
+++ lua/avimallu/lazy/eagle.nvim.lua
@@ -0,0 +1,11 @@
+return {
+  "soulis-1256/eagle.nvim",
+  opts = {
+    --override the default values found in config.lua
+  },
+  config = function()
+    require("eagle").setup({
+    --override the default values found in config.lua
+    })
+  end
+}
